2ceda1542e1f4e8815211f438ec6d6c7dd7658ae
Gustavo Mori <gustavo.mori.peixoto@usp.br>
2020-09-07 21:23:10 -0300

    add migrations, models, simple post/get

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,39 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import { getCustomRepository, getRepository } from 'typeorm'; // tratamento de dados 'interface de conexao com o banco de dados'
+import { check } from 'prettier';
+import TransactionsRepository from '../repositories/TransactionsRepository'
+import Category from '../models/Category'
+
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category}:Request ): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository)
+
+    // const checkTransactionExist = await transactionRepository.findOne({
+    //   where: { category }
+    // });
+    // if (!checkTransactionExist) {
+    //   throw new AppError('Category is invalid.')
+    // }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+    })
+
+    await transactionRepository.save(transaction)
+    return transaction;
   }
 }
 
